4a09c94992f6c971883296103c21183cc81bb882
ezalos <ezalos@gmail.com>
2021-12-16 16:42:24 +0100

    day03/ex09

---
+++ day03/ex08/other_metrics.py
@@ -0,0 +1,187 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+import numpy as np
+
+def reshape(*args):
+    new_args = []
+    for arg in args:
+        if (len(arg.shape) == 1):
+            new_args.append(arg.reshape(-1, 1))
+        else:
+            new_args.append(arg)
+    return new_args
+
+def get_metrics(y, y_hat, pos_label = 1):
+    true_positive, true_negative, false_positive, false_negative = 0, 0, 0, 0
+    for prediction, thruth in zip(y[:, 0], y_hat[:, 0]):
+        if prediction == thruth:
+            if thruth == pos_label:
+                true_positive += 1
+            else:
+                true_negative += 1
+        else:
+            if thruth == pos_label:
+                false_positive += 1
+            else:
+                false_negative += 1
+    return (true_positive, true_negative, false_positive, false_negative)
+
+def accuracy_score_(y, y_hat):
+    """
+    Compute the accuracy score.
+    Args:
+        y:a numpy.array for the correct labels
+        y_hat:a numpy.array for the predicted labels
+    Return:
+        The accuracy score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if (type(y) != type(y_hat) or type(y) != np.ndarray):
+        return None
+    y, y_hat = reshape(y, y_hat)
+    if type(y[0][0] == str):
+        pos_label = y[0][0]
+    else:
+        pos_label = 1
+    tp, tn, fp, fn = get_metrics(y, y_hat, pos_label)
+    accuracy_score = (tp + tn) / (tp + tn + fp + fn)
+    return accuracy_score
+
+def precision_score_(y, y_hat, pos_label=1):
+    """
+    Compute the precision score.
+    Args:
+        y:a numpy.array for the correct labels
+        y_hat:a numpy.array for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Return:
+        The precision score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if (type(y) != type(y_hat) or type(y) != np.ndarray or (type(pos_label) != int and type(pos_label) != str)):
+        return None
+    y, y_hat = reshape(y, y_hat)
+    tp, tn, fp, fn = get_metrics(y, y_hat, pos_label)
+    if tp == 0:
+        precision_score = 0
+    else:
+        precision_score = tp / (tp + fp)
+    return precision_score
+
+def recall_score_(y, y_hat, pos_label=1):
+    """
+    Compute the recall score.
+    Args:
+        y:a numpy.array for the correct labels
+        y_hat:a numpy.array for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Return:
+        The recall score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if (type(y) != type(y_hat) or type(y) != np.ndarray or (type(pos_label) != int and type(pos_label) != str)):
+        return None
+    y, y_hat = reshape(y, y_hat)
+    tp, tn, fp, fn = get_metrics(y, y_hat, pos_label)
+    recall_score = tp / (tp + fn)
+    return recall_score
+
+def f1_score_(y, y_hat, pos_label=1):
+    """
+    Compute the f1 score.
+    Args:
+        y:a numpy.array for the correct labels
+        y_hat:a numpy.array for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Return:
+        The f1 score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if (type(y) != type(y_hat) or type(y) != np.ndarray or (type(pos_label) != int and type(pos_label) != str)):
+        return None
+    y, y_hat = reshape(y, y_hat)
+    precision = precision_score_(y, y_hat, pos_label)
+    recall = recall_score_(y, y_hat, pos_label)
+    F1_score = (2 * precision * recall) / (precision + recall)
+    return F1_score
+
+if __name__ == "__main__":
+    print("# Example 1:")
+    y_hat = np.array([[1],[ 1],[ 0],[ 1],[ 0],[ 0],[ 1],[ 1]])
+    y = np.array([[1],[ 0],[ 0],[ 1],[ 0],[ 1],[ 0],[ 0]])
+    
+    print("# Accuracy")
+    print("## your implementation")
+    print(f"{accuracy_score_(y, y_hat) = }")
+    print("## sklearn implementation")
+    print(f"{accuracy_score(y, y_hat) = }")
+    print()
+    
+    print("# Precision")
+    print("## your implementation")
+    print(f"{precision_score_(y, y_hat) = }")
+    print("## sklearn implementation")
+    print(f"{precision_score(y, y_hat) = }")
+    print()
+
+    print("# Recall")
+    print("## your implementation")
+    print(f"{recall_score_(y, y_hat) = }")
+    print("## sklearn implementation")
+    print(f"{recall_score(y, y_hat) = }")
+    print()
+
+    print("# F1-score")
+    print("## your implementation")
+    print(f"{f1_score_(y, y_hat) = }")
+    print("## sklearn implementation")
+    print(f"{f1_score(y, y_hat) = }")
+    print()
+    
+    y_hat = np.array([[1],[ 1],[ 0],[ 1],[ 0],[ 0],[ 1],[ 1]])
+    y = np.array([[1],[ 0],[ 0],[ 1],[ 0],[ 1],[ 0],[ 0]])
+    print(f"{accuracy_score_(y, y_hat) = }")
+    print(f"{accuracy_score(y, y_hat) = }")
+    print()
+
+    print(f"{precision_score_(y, y_hat) = }")
+    print(f"{precision_score(y, y_hat) = }")
+    print()
+
+    print(f"{recall_score_(y, y_hat) = }")
+    print(f"{recall_score(y, y_hat) = }")
+    print()
+
+    print(f"{f1_score_(y, y_hat) = }")
+    print(f"{f1_score(y, y_hat) = }")
+    print()
+
+
+    y_hat = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+    y = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+    print(f"{accuracy_score_(y, y_hat) = }")
+    print(f"{accuracy_score(y, y_hat) = }")
+    print()
+
+    print(f"{precision_score_(y, y_hat, pos_label='dog') = }")
+    print(f"{precision_score(y, y_hat, pos_label='dog') = }")
+    print()
+
+    print(f"{recall_score_(y, y_hat, pos_label='dog') = }")
+    print(f"{recall_score(y, y_hat, pos_label='dog') = }")
+    print()
+
+    print(f"{f1_score_(y, y_hat, pos_label='dog') = }")
+    print(f"{f1_score(y, y_hat, pos_label='dog') = }")
+    print()
+    
---
+++ day03/ex09/confusion_matrix.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+def df_to_modify(y_source, y_to_change):
+    labels = y_source.columns
+    for column in labels:
+        if column not in y_to_change.columns:
+            y_to_change[column] = np.full((y_to_change.shape[0], ), 0)
+    y_to_change = y_to_change.reindex(columns = labels)
+    return labels, y_to_change
+
+def confusion_matrix_(y, y_hat, labels=None, df_option=False):
+    """
+    Compute confusion matrix to evaluate the accuracy of a classification.
+    Args:
+        y:a numpy.array for the correct labels
+        y_hat:a numpy.array for the predicted labels
+        labels: optional, a list of labels to index the matrix.
+        This may be used to reorder or select a subset of labels. (default=None)
+        df_option: optional, if set to True the function will return a pandas DataFrame
+        instead of a numpy array. (default=False)
+    Return:
+        The confusion matrix as a numpy array or a pandas DataFrame according to df_option value.
+        None if any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    y_dummies = pd.get_dummies(y)
+    y_hat_dummies = pd.get_dummies(y_hat)
+    if (len(y_dummies.columns) < len(y_hat_dummies.columns)):
+        all_labels, y_dummies = df_to_modify(y_hat_dummies, y_dummies)
+    else:
+        all_labels, y_hat_dummies = df_to_modify(y_dummies, y_hat_dummies)
+    if (labels != None):
+        y_hat_dummies = y_hat_dummies[labels]
+        y_dummies = y_dummies[labels]
+    else:
+        labels = all_labels
+    y = y_dummies.to_numpy()
+    y_hat = y_hat_dummies.to_numpy()
+    confusion_m = y.T @ y_hat
+    if (df_option == True):
+        df = pd.DataFrame(confusion_m, columns = labels, index = labels)
+        return df
+    return (y.T @ y_hat)
+
+
+
+if __name__ == "__main__":
+    y_hat = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'bird'])
+    y = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet'])
+
+    print("# Example 1:")
+    print("## your implementation")
+    print(f"{confusion_matrix_(y, y_hat) = }")
+    print("## sklearn implementation")
+    print(f"{confusion_matrix(y, y_hat) = }")
+    print()
+
+    print("# Example 2:")
+    print("## your implementation")
+    print(f"{confusion_matrix_(y, y_hat, labels=['dog', 'norminet']) = }")
+    print("## sklearn implementation")
+    print(f"{confusion_matrix(y, y_hat, labels=['dog', 'norminet']) = }")
+    print()
+
+    print("# Example 3:")
+    print(f"{confusion_matrix_(y, y_hat, df_option=True) = }")
+    print()
+
+    print("# Example 4:")
+    print(f"{confusion_matrix_(y, y_hat, labels=['bird', 'dog'], df_option=True) = }")
+    print()
